9cf73b39032a7ecf0e82343112b5dcc66af0ffdc
Vincent Lettau <vlettau@everstream.net>
2024-12-05 23:12:14 +0000

    closer getting tracked values

--- force-app/main/default/lwc/testComponent/testComponent.js
+++ force-app/main/default/lwc/testComponent/testComponent.js
@@ -8,19 +8,19 @@ import { getObjectInfo } from 'lightning/uiObjectInfoApi'
 export default class TestComponent extends LightningElement {
     @api recordId // Dynamically receives the record ID
     @api objectApiName // Holds the API name of the current record
-    @track fieldNames = []
+    @track fieldNames = [];
+    @track trackedfieldNames = [];
+    @track trackedFieldApiNames = [];
     @track trackedFields
-    @track trackedFieldsValues
-    // @track trackedField = {
-    //     fieldName: '',
-    //     fieldValue: '',
-    //     createdAt: ''
-    // }
+    @track trackedFieldsValues = [];
+    @track trackedFieldsValuesArray = [];
+    @track filteredValues = [];
 
-    connectedCallback() {
-        console.log('recordId', this.recordId)
-        console.log('objectApiName', this.objectApiName)
-        this.getTrackedFieldsLWC(this.objectApiName)
+
+    connectedCallback () {
+        console.log( 'recordId', this.recordId )
+        console.log( 'objectApiName', this.objectApiName )
+        this.getTrackedFieldsLWC( this.objectApiName )
             .then(
                 trackedFields => {
                     this.trackedFields = trackedFields
@@ -28,54 +28,85 @@ export default class TestComponent extends LightningElement {
             )
             .catch(
                 error => {
-                    console.log('connectedCallback getTrackedFieldsLWC error', error)
+                    console.log( 'connectedCallback getTrackedFieldsLWC error', error )
                 }
             )
     }
 
-    @wire(getObjectInfo, { objectApiName: '$objectApiName' }) // Fetch object metadata
-    wiredObjectInfo({ data, error }) {
-        if (data) {
+    @wire( getObjectInfo, { objectApiName: '$objectApiName' } ) // Fetch object metadata
+    wiredObjectInfo ( { data, error } ) {
+        if ( data ) {
             const fields = data.fields
-            for (const fieldName in fields) {
-                if (fields.hasOwnProperty(fieldName)) {
-                    const field = this.objectApiName + '.' + fields[fieldName].apiName
-                    console.log('field', field)
-                    this.fieldNames.push(field)
+            for ( const fieldName in fields ) {
+                if ( fields.hasOwnProperty( fieldName ) ) {
+                    const fieldApi = this.objectApiName + '.' + fields[ fieldName ].apiName
+                    for ( const trackedField of this.trackedFieldApiNames ) {
+                        // if ( this.trackedFieldApiNames.includes( fieldApi ) ) {
+                        //     console.log( 'fieldApi', fieldApi )
+                        //     this.fieldNames.push( fieldApi )
+                        // }
+                        if ( trackedField === fieldApi ) {
+                            console.log( 'fieldApi', fieldApi )
+                            this.fieldNames.push( fieldApi )
+                        }
+                    }
                 }
             }
-            console.log('this.fieldNames', this.fieldNames)
-            console.log('wiredObjectInfo data', data)
-        } else if (error) {
-            console.log('wiredObjectInfo error', error)
+            // for ( const trackedField of this.trackedFieldApiNames ) {
+            //     console.log( 'trackedField', trackedField )
+            // }
+            // Filter out the tracked fields from the fieldNames array
+            // this.filteredValues = this.fieldNames.filter( field => !this.trackedFieldApiNames.includes( field ) )
+            // console.log( 'filteredValues', this.filteredValues )
+        } else if ( error ) {
+            console.log( 'wiredObjectInfo error', error )
         }
     }
 
-    @wire(getRecord, { recordId: '$recordId', fields: '$fieldNames' })
-    wiredRecord({ data, error }) {
-        if (data) {
-            console.log('wiredRecord getRecord data', data)
-        } else if (error) {
-            console.log('wiredRecord getRecord error', error)
+    @wire( getRecord, { recordId: '$recordId', fields: '$fieldNames' } )
+    wiredRecord ( { data, error } ) {
+        if ( data ) {
+            console.log( 'data', data )
+        } else if ( error ) {
+            console.log( 'wiredRecord getRecord error', error )
         }
     }
 
-    async getTrackedFieldsLWC(objectApiName) {
+    async getTrackedFieldsLWC ( objectApiName ) {
         try {
-            const data = await getTrackedFields({ objectApiName })
-            if (data && data.length > 0) {
-                const trackedFields = data[0]?.[TRACKED_FIELDS_DATA.fieldApiName] || []
-                this.configRecordId = data[0]?.Id
-                const trackecFieldsValues = await getTrackedFieldsValues({ objectApiName: this.objectApiName, fieldNames: trackedFields, recordId: this.recordId })
-                this.trackedFieldsValues = trackecFieldsValues.Account__c.value
-                console.log('tracked fields Account: ', this.trackedFieldsValues)
+            const data = await getTrackedFields( { objectApiName } )
+            if ( data && data.length > 0 ) {
+                const trackedFields = data[ 0 ]?.[ TRACKED_FIELDS_DATA.fieldApiName ] || []
+                const trackedFieldsValues = await getTrackedFieldsValues( {
+                    objectApiName: this.objectApiName,
+                    fieldNames: trackedFields,
+                    recordId: this.recordId
+                } )
+                try {
+                    for ( const trackedValue in trackedFieldsValues ) {
+                        if ( typeof trackedValue !== 'undefined' && !trackedValue.includes( 'undefined' ) && !trackedValue.includes( 'null' ) ) {
+                            console.log( ' non-null tracked values: ', trackedValue )
+                            const fieldApi = this.objectApiName + '.' + trackedValue
+                            console.log( 'fieldApi', fieldApi )
+                            this.trackedFieldApiNames.push( fieldApi )
+                        }
+                        else {
+                            console.log( 'any tracked values: ', trackedValue )
+                        }
+                    }
+                    this.trackedFieldApiNames.forEach( field => {
+                        console.log( 'trackedFieldApiNames', field )
+                    } )
+                } catch ( error ) {
+                    console.log( 'getTrackedFieldsLWC loop error', error )
+                }
                 return trackedFields
             } else {
-                console.warn('No tracked fields data found.')
+                console.warn( 'No tracked fields data found.' )
                 return []
             }
-        } catch (error) {
-            console.error('Error fetching tracked fields:', error)
+        } catch ( error ) {
+            console.error( 'Error fetching tracked fields:', error )
             return []
         }
     }
