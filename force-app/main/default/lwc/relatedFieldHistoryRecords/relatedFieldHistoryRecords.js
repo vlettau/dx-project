f36e436cfd24082832ea2fc5109b2809e7a9f067
Vincent Lettau <vlettau@everstream.net>
2024-12-08 03:39:11 +0000

    everthing is working all parts! cant believe it...

--- force-app/main/default/lwc/relatedFieldHistoryRecords/relatedFieldHistoryRecords.js
+++ force-app/main/default/lwc/relatedFieldHistoryRecords/relatedFieldHistoryRecords.js
@@ -1,31 +1,27 @@
 import { LightningElement, api, wire, track } from 'lwc'
-import { getRelatedListRecords } from 'lightning/uiRelatedListApi'
+import getFieldHistoryRecords from '@salesforce/apex/FieldHistorySettingsController.getFieldHistoryRecords'
 
 export default class RelatedFieldHistoryRecords extends LightningElement {
 
     @api recordId
     @track relatedListRecords = []
 
-    @wire( getRelatedListRecords, {
-        parentRecordId: '$recordId',
-        relatedListId: 'Fields_Historys__r',
-        fields: [
-            'Field_History__c.CreatedDate',
-            'Field_History__c.CreatedBy.Name',
-            'Field_History__c.Field_API_Name__c',
-            'Field_History__c.Original_Value__c',
-            'Field_History__c.New_Value__c'
-        ],
-        sortBy: 'Field_History__c.CreatedDate',
-        sortDirection: 'DESC'
-    } )
-    wiredRelatedList ( { data, error } ) {
-        if ( data ) {
-            const relatedListRecords = data
-            console.log( 'wiredRelatedList', relatedListRecords )
-        } else if ( error ) {
-            console.error( 'Error fetching related list:', error )
-        }
+    connectedCallback () {
+        this.getFieldHistoryRecordsLWC( this.recordId )
+            .then(
+                data => {
+                    this.relatedListRecords = data
+                }
+            )
+            .catch(
+                error => {
+                    console.error( 'Error fetching field history records:', error )
+                }
+            )
+    }
+
+    async getFieldHistoryRecordsLWC ( recordId ) {
+        return await getFieldHistoryRecords( { recordId: recordId } )
     }
 
 }
--- force-app/main/default/lwc/testComponent/testComponent.js
+++ force-app/main/default/lwc/testComponent/testComponent.js
@@ -1,115 +1,98 @@
-import { LightningElement, api, track, wire } from 'lwc'
-import { getRecord } from 'lightning/uiRecordApi'
-import getTrackedFields from '@salesforce/apex/FieldHistorySettingsController.getTrackedFields'
-import getTrackedFieldsValues from '@salesforce/apex/FieldHistorySettingsController.getTrackedFieldsValues'
-import TRACKED_FIELDS_DATA from '@salesforce/schema/Field_History_Setting__c.Tracked_Fields_Data__c'
-import getFieldHistoryRecord from '@salesforce/apex/FieldHistorySettingsController.getFieldHistoryRecord'
+import { LightningElement, api, track, wire } from 'lwc';
+import { createRecord, updateRecord } from 'lightning/uiRecordApi';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getTrackedFields from '@salesforce/apex/FieldHistorySettingsController.getTrackedFields';
+
 
 export default class TestComponent extends LightningElement {
-    @api recordId // Dynamically receives the record ID
-    @api objectApiName // Holds the API name of the current record
-    @track fieldNames = [];
+    @api recordId;
+    @api objectApiName;
+    @track fieldsInfo = [];
     @track fieldSet = new Set();
+    @track currentTrackedFields = [];
+    @track hasSettingsRecord = false;
+    @track configRecordId = '';
+    @track isLoading = false;
 
-    connectedCallback () {
-        console.log( 'recordId', this.recordId )
-        console.log( 'objectApiName', this.objectApiName )
-        this.getTrackedFieldsLWC( this.objectApiName )
-            .then(
-                trackedFields => {
-                    getTrackedFieldsValues( {
-                        objectApiName: this.objectApiName,
-                        fieldNames: trackedFields,
-                        recordId: this.recordId
-                    } )
-                        .then(
-                            trackedFieldsValues => {
-                                for ( const trackedValue in trackedFieldsValues ) {
-                                    console.log( 'trackedValue', trackedValue )
-                                    if ( typeof trackedValue !== 'undefined' && !trackedValue.includes( 'undefined' ) && !trackedValue.includes( 'null' ) ) {
-                                        const fieldApi = this.objectApiName + '.' + trackedValue
-                                        console.log( 'full field Api', fieldApi )
-                                        this.fieldSet.add( fieldApi )
-                                    }
-                                    else {
-                                        console.log( 'any tracked values: ', trackedValue )
-                                    }
-                                }
-                                console.log( 'this.fieldSet', this.fieldSet )
-                                this.fieldNames = [ ...this.fieldSet ]
-                                console.log( 'this.fieldNames', this.fieldNames )
-                            }
-                        )
-                        .catch(
-                            error => {
-                                console.log( 'connectedCallback getTrackedFieldsLWC error', error )
-                            }
-                        )
-                }
-            )
-            .catch(
-                error => {
-                    console.log( 'connectedCallback getTrackedFieldsLWC error', error )
-                }
-            )
-    }
-
-    @wire( getRecord, { recordId: '$recordId', fields: '$fieldNames' } )
-    wiredRecord ( { data, error } ) {
-        if ( data ) {
-            console.log( 'data', data )
-            const fields = data.fields
-            for ( const field in fields ) {
-                console.log( 'this.objectApiName', this.objectApiName )
-                console.log( 'field', field )
-                console.log( 'fields[ field ]', fields[ field ].value )
-                console.log( 'this.recordId', this.recordId )
-                this.getFieldHistoryRecordLWC( this.objectApiName, field, fields[ field ].value, this.recordId )
-                    .then(
-                        data => {
-                            console.log( 'getFieldHistoryRecordLWC data', data )
-                        }
-                    ).catch(
-                        error => {
-                            console.log( 'getFieldHistoryRecordLWC error', error )
-                        }
-                    )
-            }
-            console.log( 'fields', fields )
-        } else if ( error ) {
-            console.log( 'wiredRecord getRecord error', error )
+    @wire(getObjectInfo, { objectApiName: '$objectApiName' })
+    wiredObjectInfo({ data, error }) {
+        if (data) {
+            this.fieldsInfo = Object.keys(data.fields).map(fieldName => {
+                const field = data.fields[fieldName];
+                return {
+                    fieldName: field.apiName,
+                    label: field.label,
+                    isTracked: this.currentTrackedFields.includes(field.apiName),
+                };
+            });
+            console.log('Fields Info:', this.fieldsInfo);
+        } else if (error) {
+            console.error('Error fetching object info:', error);
         }
     }
 
-    async getTrackedFieldsLWC ( objectApiName ) {
+    async connectedCallback() {
+        this.isLoading = true;
         try {
-            const data = await getTrackedFields( { objectApiName } )
-            if ( data && data.length > 0 ) {
-                const trackedFields = data[ 0 ]?.[ TRACKED_FIELDS_DATA.fieldApiName ] || []
-                return trackedFields
-            } else {
-                console.warn( 'No tracked fields data found.' )
-                return []
-            }
-        } catch ( error ) {
-            console.error( 'Error fetching tracked fields:', error )
-            return []
+            const trackedFields = await this.getTrackedFieldsLWC(this.objectApiName);
+            this.currentTrackedFields = trackedFields;
+            this.hasSettingsRecord = trackedFields.length > 0;
+        } catch (error) {
+            console.error('Error fetching tracked fields:', error);
+        } finally {
+            this.isLoading = false;
         }
     }
 
-    async getFieldHistoryRecordLWC ( objectApiName, fieldName, fieldValue, recordId ) {
+    async getTrackedFieldsLWC(objectApiName) {
         try {
-            console.log( 'getFieldHistoryRecordLWC objectApiName', objectApiName )
-            console.log( 'getFieldHistoryRecordLWC fieldName', fieldName )
-            console.log( 'getFieldHistoryRecordLWC fieldValue', fieldValue )
-            console.log( 'getFieldHistoryRecordLWC recordId', recordId )
-            const data = await getFieldHistoryRecord( { objectApiName: objectApiName, fieldName: fieldName, fieldValue: fieldValue, recordId: recordId } )
-            console.log( 'getFieldHistoryRecordLWC data', data )
-            return data
-        } catch ( error ) {
-            console.error( 'getFieldHistoryRecordLWC error', error )
-            return []
+            const data = await getTrackedFields({ objectApiName });
+            return data.length > 0 ? data[0]?.[TRACKED_FIELDS_DATA.fieldApiName] || [] : [];
+        } catch (error) {
+            console.error('Error fetching tracked fields:', error);
+            return [];
         }
     }
 
+    handleCheckboxChange(event) {
+        const fieldName = event.target.dataset.id;
+        const isTracked = event.target.checked;
+
+        if (isTracked) {
+            this.fieldSet.add(fieldName);
+        } else {
+            this.fieldSet.delete(fieldName);
+        }
+
+        console.log('Updated Field Set:', [...this.fieldSet]);
+    }
+
+    saveFieldHistorySettings(event) {
+        event.preventDefault();
+        this.isLoading = true;
+
+        const mergedFields = [...this.fieldSet];
+        const fields = {
+            Object_API_Name__c: this.objectApiName,
+            Tracked_Fields_Data__c: JSON.stringify(mergedFields),
+        };
+
+        if (this.hasSettingsRecord) {
+            fields.Id = this.configRecordId;
+            updateRecord({ fields })
+                .then(() => this.showToast('Success', 'Settings updated successfully!', 'success'))
+                .catch(error => this.showToast('Error', 'Failed to save settings.', 'error'))
+                .finally(() => (this.isLoading = false));
+        } else {
+            createRecord({ apiName: 'Field_History_Setting__c', fields })
+                .then(() => this.showToast('Success', 'Settings saved successfully!', 'success'))
+                .catch(error => this.showToast('Error', 'Failed to save settings.', 'error'))
+                .finally(() => (this.isLoading = false));
+        }
+    }
+
+    showToast(title, message, variant) {
+        this.dispatchEvent(new ShowToastEvent({ title, message, variant }));
+    }
 }
